93ee01d986c12a83232a49428a1da73e9bcf0852
mwlodarz <wlodarzmar@gmail.com>
2017-05-30 06:22:21 +0200

    init project

---
+++ UsersOnline-SignalR/UsersOnline-SignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsersOnline_SignalR.Startup))]
+namespace UsersOnline_SignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
